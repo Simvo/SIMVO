9e92eba341a5c064f8743fe0d804da145fff13dd
thomas karatzas <thomas.karatzas@mail.mcgill.ca>
2016-05-02 14:40:25 -0400

    First commit on dev branch, added folders for views and started the master view

---
+++ resources/views/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php session()->regenerate(); ?>
+    <meta charset="utf-8">
+    <meta name="_token" content="{!! csrf_token() !!}"/>
+    <meta property="og:title" content="S!MVO McGill" />
+    <meta property="og:description" content="S!MVO Allows students to plan their degrees using an intuitive web application. S!mvo was developed by students at McGill Univeristy." />
+    <meta property="og:image" content="http://www.international.gouv.qc.ca/Content/Users/Documents/FicheContenu/263.jpg"/>
+  	<meta property="og:type" content="website"/>
+    <title>S!MVO</title>
+  </head>
+</html>
