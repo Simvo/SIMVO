111f5180abdf06f9a342ed8103e47ab6a241f24e
Thomas Wong <tomaswolfgang@gmail.com>
2016-05-14 21:59:30 -0400

    Loading majors form error/return navigation
    copied code to execute at page load (in addition to the .change
    function)

--- public/js/registrationPage.js
+++ public/js/registrationPage.js
@@ -6,6 +6,30 @@ $(document).ready(function(){
     }
   } );
 
+  var selectedFaculty = $('#faculty-select option:selected').text();
+
+  $('#major-select').empty();
+
+  if(selectedFaculty !== "None")
+  {
+    $.ajax({
+      type: 'post',
+      url: '/auth/registration/get-majors',
+      data: { faculty : selectedFaculty},
+      success: function(data) {
+
+        var response = JSON.parse(data);
+
+        for(var i =0; i<response.length; i++)
+        {
+          var option = '<option value="'+response[i][1]+'">' + response[i][0]+ '</option>';
+
+          $('#major-select').append(option);
+        }
+      }
+    })
+  }
+
   $('#faculty-select').change(function(){
 
     var selectedFaculty = $('#faculty-select option:selected').text();
