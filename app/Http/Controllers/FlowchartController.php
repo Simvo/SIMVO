a611f2227cf1a4c411625fc61d795387e4128296
thomas karatzas <thomas.karatzas@mail.mcgill.ca>
2016-06-04 12:22:42 -0700

    parsing is working, need to find efficient way to elimate unwanted groups

--- app/Http/Controllers/FlowchartController.php
+++ app/Http/Controllers/FlowchartController.php
@@ -15,33 +15,8 @@ class FlowchartController extends Controller
   use Traits\ProgramTrait;
   use Traits\tools;
 
-  /*TEST VIEW*/
-  /* THIS view is purely for testing the regex on getting group credits*/
-  //just copied and pasted the following options to test
-  use Traits\ParsingTrait;
-  public function test(){
-    /*possible options for input from the database:
-      "3 credits from the following:"
-      "3 credits (6 credits for students from Quebec CEGE..."
-      "24 credits"
-      "9 creditsThe purpose of this requirement is to pro..."
-      "List B0-6 credits from the following:"
-      "29 creditsGenerally, students admitted to Engineer..."
-      "If you have successfully completed a course at CEG..."
-      "If you are not proficient in a certain language, n..."
-      "74 credits"
-      "A Chemical Engineering student may complete the Bi..."
-      NULL
-      "Note: Engineering students may not use these cours..."
-      "12 credits"
-      "12 credits selected from courses outside the Depar..."
-      "15 credits from the following lists, two courses o..."
-      */
-    return $this->extractCreditFromDesc("29 creditsGenerally, students admitted to Engineer...");
-  }
-
     /**
-    * Function called upon GET request. Will determine if shedule needs to be generated or simply displayed
+    * Function called upon GET request. Will determine if schedule needs to be generated or simply displayed
     * Consists of generating four main parts.
     * 1) Progress Bar 2)Course Schedule 3) Complementary Courses 4) Elecitive Courses
     **/
@@ -79,6 +54,4 @@ class FlowchartController extends Controller
 
       return $progress;
     }
-
-
 }
--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
+            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
             'strict' => false,
             'engine' => null,
         ],
