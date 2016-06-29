53ab3ade482f4277a17d4e4b051f522838b1d319
thomas karatzas <thomas.karatzas@mail.mcgill.ca>
2016-06-28 19:54:54 -0700

    Structure for vsb implimentation added

---
+++ app/Http/Controllers/CurlController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class CurlController extends Controller
+{
+  use Traits\NewObjectsTrait;
+  use Traits\ProgramTrait;
+  use Traits\tools;
+
+  public function createVSBSchedule($semester)
+  {
+
+  }
+
+  public function checkCourseAvailablity($course, $semester)
+  {
+
+  }
+}
