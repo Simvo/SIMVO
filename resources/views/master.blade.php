83e3dee41a3cfb07516ff187267e731e1c2467dd
tristanrenondin <tristanrenondin@gmail.com>
2016-10-03 18:45:55 -0400

    Update master.blade.php
    Adding people data for mixpanel

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -37,7 +37,14 @@
       mixpanel.identify("12148");
 
       mixpanel.people.set({
-
+        "$email": "jsmith@example.com",    // only special properties need the $
+    
+    "$created": "2011-03-16 16:53:54",
+    "$last_login": new Date(),         // properties can be dates...
+    
+    "credits": 150,                    // ...or numbers
+    
+    "gender": "Male"                    // feel free to define your own prop
       });
     </script>
   </head>
