70aefea49c085c68439061f972ba34962fa596a5
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2016-12-16 11:59:50 -0500

    added 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Be right back.</title>
+
+        <link href="/css/404.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="text-wrapper">
+            <div class="title" data-content="404">
+                404
+            </div>
+
+            <div class="subtitle" data-content="Oops, the page you're looking for doesn't exist">
+                Oops, the page you're looking for doesn't exist.
+            </div>
+
+            <div class="buttons">
+                <a class="button" href="{{ route('flowchart') }}">Go to homepage</a>
+            </div>
+        </div>
+    </body>
+</html>
