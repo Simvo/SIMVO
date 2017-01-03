4f3357db24633e29ad779c993a0fd03655357d00
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-02 19:16:17 -0500

    Finished SOS

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -77,36 +77,76 @@
     </div>
     <div class="container">
       <div class="row">
-        <div class="col-lg-4 col-md-4 text-center">
+        <div class="col-lg-12 col-md-12 text-center">
           <div class="feature">
             <i class="icon-lg ion-android-laptop wow fadeIn" data-wow-delay=".3s"></i>
             <h3>Visualize Your Degree</h3>
-            <p class="text-muted">Get started by Choosing your Major And Stream</p>
+            <p class="text-muted" style="margin-bottom: 10px">Choose your Major And Stream. Information Comes directly from McGill ensuring accuracy.</p>
           </div>
+          <video autoplay="" loop="true" class="" data-wow-delay="0.5s" poster="https://s3-us-west-2.amazonaws.com/coverr/poster/Traffic-blurred2.jpg"
+            id="demo-vid">
+            <source src="/media/SimvoVisualizeDegree.mov" type="video/mp4">Your browser does not support the video tag. I suggest you upgrade your browser.
+          </video>
         </div>
-        <div class="col-lg-4 col-md-4 text-center">
+        <div class="col-lg-6 col-md-12 text-center">
           <div class="feature">
             <i class="icon-lg ion-social-sass wow fadeInUp" data-wow-delay=".2s"></i>
             <h3>Preview Semesters</h3>
             <p class="text-muted">Drag courses into your current semester and quickly see wht your schedule will look like.</p>
           </div>
+          <video autoplay="" loop="true" class="" data-wow-delay="0.5s" poster="https://s3-us-west-2.amazonaws.com/coverr/poster/Traffic-blurred2.jpg"
+            id="demo-vid">
+            <source src="/media/SimvoPreviewSched.mov" type="video/mp4">Your browser does not support the video tag. I suggest you upgrade your browser.
+          </video>
         </div>
-        <div class="col-lg-4 col-md-4 text-center">
+        <div class="col-lg-6 col-md-12 text-center col-centered">
           <div class="feature">
             <i class="icon-lg ion-ios-star-outline wow fadeIn" data-wow-delay=".3s"></i>
             <h3>Tweak to Your Liking!</h3>
-            <p class="text-muted">Add a Minor, Visualize your graduation date if you take a semester off.</p>
+            <p class="text-muted">Visualize your graduation date if you add a Minor, do an internship or a semester off.</p>
           </div>
+          <video autoplay="" loop="true" class="" data-wow-delay="0.5s" poster="https://s3-us-west-2.amazonaws.com/coverr/poster/Traffic-blurred2.jpg"
+            id="demo-vid">
+            <source src="/media/SimvoAddInternship.mov" type="video/mp4">Your browser does not support the video tag. I suggest you upgrade your browser.
+          </video>
         </div>
       </div>
     </div>
   </section>
-  <section id="two">
+  <section id="two" class="SOS-tab">
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
-          <h2 class="margin-top-0 text-primary">The Students of S!MVO Program</h2>
+          <h2 class="margin-top-0 text-primary">Students of S!MVO</h2>
           <hr class="primary">
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 text-center">
+          <h2 class="text-primary" style="margin-bottom: 20px;">What is the Students of S!MVO Program?</h2>
+          <p class="primary-text text-faded">The Students of S!MVO program is a 1 term educational 
+          program that will select 15-20 student applicants to develop and implement 
+          multiple S!MVO projects that focus on improving some aspect of McGill. 
+          Students will be split into dedicated project groups whilst being guided, 
+          trained and mentored throughout the semester. The projects available for 
+          this winter range from electronic financial modeling, to building a group 
+          study finder app, 
+          to writing and publishing an academic paper.</p>
+        </div>
+        <div class="col-lg-2 text-center"></div>
+        <div class="col-lg-5 text-center">
+          <h2 class="text-primary" style="margin-bottom: 20px;">Who Is The Program Suited for?</h2>
+          <p class="primary-text text-faded">The Students of S!MVO program is McGill’s ideal opportunity to earn invaluable experience and develop a unique mindset critical for students wishing:
+            To become valuable future employees with unique corporate and interpersonal skills
+
+            Or especially Those wishing to become future entrepreneurs with their own idea
+
+            Join a large group of motivated like-minded individuals, develop and share your experiences, and make an impact on your peers by enrolling in our Students of S!MVO program. 
+
+            If you are selected, you along with your team will be taking on one the following S!MVO projects:
+          </p>
         </div>
       </div>
     </div>
