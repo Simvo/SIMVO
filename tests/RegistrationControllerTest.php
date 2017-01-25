9876cd11fc9f4084f65d9033656d1b62e4684e3c
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-25 11:37:17 -0500

    hopefully this one works

--- tests/RegistrationControllerTest.php
+++ tests/RegistrationControllerTest.php
@@ -11,16 +11,22 @@ class RegistrationControllerTest extends TestCase
      *
      * @return void
      */
+    
+    public function migrate()
+    {
+        exec("php artisan migrate");
+    }
+
     public function testUserRegistrationValid()
     {
-         $this->visit('/auth/registration')
-              ->see("First, Let's Get Your McGill E-mail and Password for Your New Account.")
-              ->type('thomas.karatzas@mail.mcgill.ca', 'Email')
-              ->type('Thomas', 'First_Name')
-              ->type('Karatzas', 'Last_Name')
-              ->type('8characterPass', 'Password')
-              ->type('8characterPass', 'Confirm_Password')
-              ->press('Submit')
-              ->seePageIs('/flowchart');
+        $this->visit('/auth/registration')
+            ->see("First, Let's Get Your McGill E-mail and Password for Your New Account.")
+            ->type('thomas.karatzas@mail.mcgill.ca', 'Email')
+            ->type('Thomas', 'First_Name')
+            ->type('Karatzas', 'Last_Name')
+            ->type('8characterPass', 'Password')
+            ->type('8characterPass', 'Confirm_Password')
+            ->press('Submit')
+            ->seePageIs('/flowchart');
     }
 }
