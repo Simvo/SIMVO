76285fa414eeecc4b2f1a11bd7e142ccec9fa678
Thomas Karatzas <thomas.karatzas@mail.mcgill.ca>
2017-01-26 10:13:08 -0500

    using mysql in travis

--- config/database.php
+++ config/database.php
@@ -67,11 +67,27 @@ return [
             'engine' => null,
         ],
 
-         'testing' => [
+         'testingSQLite' => [
             'driver' => 'sqlite',
             'database' => ':memory:',
             'prefix' => '',
         ],
+
+        'travisSQL' => [
+           'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost:8000'),
+            'port' => env('DB_PORT', '8000'),
+            'database' => env('DB_DATABASE', 'SIMVO'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            //'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
 
         'pgsql' => [
             'driver' => 'pgsql',
